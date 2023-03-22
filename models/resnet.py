6da6e47687bd4b654fd33baf80f2b26a6cc1dd64
gkgo <91817417+gkgo@users.noreply.github.com>
2023-03-22 16:40:29 +0800

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -1,9 +1,59 @@
+import torch.nn.functional as F
+import torch
 import torch.nn as nn
 
-# This ResNet network was designed following the practice of the following papers:
-# TADAM: Task dependent adaptive metric for improved few-shot learning (Oreshkin et al., in NIPS 2018) and
-# A Simple Neural Attentive Meta-Learner (Mishra et al., in ICLR 2018).
+def featureL2Norm(feature):
+    epsilon = 1e-6
+    norm = torch.pow(torch.sum(torch.pow(feature, 2), 1) + epsilon, 0.5).unsqueeze(1).expand_as(feature)
+    return torch.div(feature, norm)
 
+class mySelfCorrelationComputation(nn.Module):
+    def __init__(self, kernel_size=(5, 5), padding=2):
+        super(mySelfCorrelationComputation, self).__init__()
+        planes =[640, 64, 64, 640]
+        self.kernel_size = kernel_size
+        self.unfold = nn.Unfold(kernel_size=kernel_size, padding=padding)
+        self.relu = nn.ReLU(inplace=False)
+
+        self.conv1x1_in = nn.Sequential(nn.Conv2d(planes[0], planes[1], kernel_size=1, bias=False, padding=0),
+                                        nn.BatchNorm2d(planes[1]),
+                                        nn.ReLU(inplace=True))
+        self.embeddingFea = nn.Sequential(nn.Conv2d(1664, 640,
+                                                     kernel_size=1, bias=False, padding=0),
+                                           nn.BatchNorm2d(640),
+                                           nn.ReLU(inplace=True))
+        self.conv1x1_out = nn.Sequential(
+            nn.Conv2d(640, 640, kernel_size=1, bias=False, padding=0),
+            nn.BatchNorm2d(640))
+
+    def forward(self, x):
+
+        x = self.conv1x1_in(x)
+        b, c, h, w = x.shape
+
+        x0 = self.relu(x)
+        x = x0
+        x = F.normalize(x, dim=1, p=2)
+        identity = x
+
+        x = self.unfold(x)  # 提取出滑动的局部区域块，这里滑动窗口大小为5*5，步长为1
+        # b, cuv, h, w  （80,640*5*5,5,5)
+        x = x.view(b, c, self.kernel_size[0], self.kernel_size[1], h, w)  # b, c, u, v, h, w
+        x = x * identity.unsqueeze(2).unsqueeze(2)  # 通过unsqueeze增维使identity和x变为同维度  公式（1）
+        # b, c, u, v, h, w * b, c, 1, 1, h, w
+        x = x.view(b, -1, h, w)
+        # x = x.permute(0, 1, 4, 5, 2, 3).contiguous()  # b, c, h, w, u, v
+        # torch.contiguous()方法首先拷贝了一份张量在内存中的地址，然后将地址按照形状改变后的张量的语义进行排列
+        # x = x.mean(dim=[-1, -2])
+        feature_gs = featureL2Norm(x)
+
+        # concatenate
+        feature_cat = torch.cat([identity, feature_gs], 1)
+
+        # embed
+        feature_embd = self.embeddingFea(feature_cat)
+        feature_embd = self.conv1x1_out(feature_embd)
+        return feature_embd
 
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
@@ -61,6 +111,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 160, stride=2)
         self.layer3 = self._make_layer(block, 320, stride=2)
         self.layer4 = self._make_layer(block, 640, stride=2)
+        self.scr_module = mySelfCorrelationComputation(kernel_size=(5, 5), padding=2)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -92,5 +143,12 @@ class ResNet(nn.Module):
         x = self.layer3(x)
 
         x = self.layer4(x)
+        
+        identity = x
+
+        x = self.scr_module(x)
+
+
+        x = x + identity
 
         return x
