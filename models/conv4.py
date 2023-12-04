db193bcd1e639258af69b1a56b8fbe4cb7442875
gkgo <91817417+gkgo@users.noreply.github.com>
2023-12-04 17:08:48 +0800

    Update conv4.py

--- models/conv4.py
+++ models/conv4.py
@@ -1,6 +1,6 @@
 # import torch.nn as nn
-#
-#
+
+
 # def conv_block(in_channels, out_channels):
 #     return nn.Sequential(
 #         nn.Conv2d(in_channels, out_channels, 3, padding=1),
@@ -8,9 +8,9 @@
 #         nn.ReLU(),
 #         nn.MaxPool2d(2)
 #     )
-#
+
 # class ConvNet4(nn.Module):
-#
+
 #     def __init__(self,args, x_dim=3, hid_dim=64, z_dim=640):
 #         super().__init__()
 #         self.args = args
@@ -20,7 +20,7 @@
 #             conv_block(hid_dim, hid_dim),
 #             conv_block(hid_dim, z_dim),
 #         )
-#
+
 #     def forward(self, x):
 #         x = self.encoder(x)
 #         return x
